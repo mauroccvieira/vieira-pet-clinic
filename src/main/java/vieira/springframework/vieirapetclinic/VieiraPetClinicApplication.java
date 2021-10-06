d84b6aa2f14a35593a54676b160ad135c8d06d79
Mauro Vieira <mauro@contasimples.com>
2021-10-06 08:48:37 -0300

    Initial commit

---
+++ src/main/java/vieira/springframework/vieirapetclinic/VieiraPetClinicApplication.java
@@ -0,0 +1,13 @@
+package vieira.springframework.vieirapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VieiraPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VieiraPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vieira/springframework/vieirapetclinic/VieiraPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package vieira.springframework.vieirapetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VieiraPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
